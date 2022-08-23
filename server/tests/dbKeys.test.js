4cb10ee1ac55db95292d21c19661e68acc33798e
trent-cooper <00trent@protonmail.com>
2022-08-23 16:35:58 -0500

    Addedtesting coverage to DB logs and SDK key operations.
    Co-authored-by: Steven Liou <stevenliou@gmail.com>
    Co-authored-by: Jordan Eggers <jordan.shay.eggers@gmail.com>
    Co-authored-by: Elaine Vuong <e.vuong94@gmail.com>

---
+++ server/tests/dbKeys.test.js
@@ -0,0 +1,68 @@
+const { Client } = require('pg');
+
+const {
+  setupDatabase,
+  createTable,
+  dropDatabase,
+  tableExistsQuery,
+  projectConfig,
+  schema,
+  dropTable,
+} = require('../lib/db/setup/setupHelpers');
+
+const {
+  createKey,
+  getKey,
+  endPoolConnection,
+} = require('../lib/db');
+
+let client;
+
+beforeAll(async () => {
+  await setupDatabase(projectConfig);
+  client = new Client(projectConfig);
+  await client.connect();
+});
+
+beforeEach(async () => {
+  await createTable(client, { name: 'keys', query: schema.keys });
+})
+
+afterEach(async () => {
+  await dropTable(client, 'keys')
+})
+
+afterAll(async () => {
+  await client.end();
+  await endPoolConnection();
+  await dropDatabase(projectConfig);
+});
+
+describe('SDK Key table', () => {
+  it('exists', async () => {
+    let res = await client.query(tableExistsQuery('keys'));
+    expect(res.rowCount).toBe(1);
+  });
+
+  it('can create a key', async () => {
+    let keyCountQuery = `SELECT * FROM keys;`;
+    let res = await client.query(keyCountQuery);
+    expect(res.rowCount).toBe(0);
+
+    res = await createKey();
+    res = await client.query(keyCountQuery);
+    expect(res.rowCount).toBe(1);
+  });
+
+  it('can get key', async () => {
+    let keyCountQuery = `SELECT * FROM keys;`;
+    let res = await client.query(keyCountQuery);
+    expect(res.rowCount).toBe(0);
+
+    res = await createKey();
+
+    res = await getKey()
+    expect(res.rows[0]).toHaveProperty('id')
+  })
+
+});
