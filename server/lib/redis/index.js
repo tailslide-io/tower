778039e2e960e2cce5b13aaf6be239b9d8b17b5d
Elaine Vuong <elaine.yt.vuong@gmail.com>
2022-08-25 15:52:51 -0400

    Updated app to serve from the Build directory, and modified Redis connection
    Co-authored-by: Steven Liou <stevenliou@gmail.com>
    Co-authored-by: Trent Cooper <00trent@protonmail.com>
    Co-authored-by: Jordan Eggers <jordan.shay.eggers@gmail.com>

--- server/lib/redis/index.js
+++ server/lib/redis/index.js
@@ -1,7 +1,8 @@
 const RedisTimeSeriesClient = require('./redisTimeSeriesClient');
 
 module.exports = (async () => {
-  const redisClient = new RedisTimeSeriesClient(JSON.parse(process.env.REDIS_SERVER) || 'http://localhost:6379');
+  const redisAddress = process.env.REDIS_SERVER || null;
+  const redisClient = new RedisTimeSeriesClient(redisAddress);
   await redisClient.init();
   return redisClient;
 })();
